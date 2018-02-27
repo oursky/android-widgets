9e3f168d17de3f7ecffdb315d265a6c2b0c47e2e
hidden-dimensions <hidden-dimensions@users.noreply.github.com>
2018-02-27 22:44:52 +0800

    refs #1 code skeleton

---
+++ widgets/src/main/java/com/oursky/widget/DummyView.kt
@@ -0,0 +1,10 @@
+package com.oursky.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DummyView : View {
+    constructor(context: Context): super(context)
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+}
