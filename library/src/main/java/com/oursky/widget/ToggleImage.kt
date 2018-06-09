6f07be41adbb350c13fd793142b6c090a5b85772
hidden-dimensions <hidden-dimensions@users.noreply.github.com>
2018-06-09 20:04:05 +0800

    #8 ToggleImage deem effect

--- library/src/main/java/com/oursky/widget/ToggleImage.kt
+++ library/src/main/java/com/oursky/widget/ToggleImage.kt
@@ -1,7 +1,9 @@
 package com.oursky.widget
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.widget.Checkable
 import android.widget.ImageView
 
@@ -10,21 +12,64 @@ class ToggleImage : ImageView, Checkable {
     // delegates
     var onToggle: ((ToggleImage, Boolean) -> Unit)? = null
     private var mChecked: Boolean = false
+    private var mPressed: Boolean = false
+    private val mAlphaAnimator: ObjectAnimator
 
     constructor(context: Context) : this(context, null, 0)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         scaleType = ImageView.ScaleType.FIT_CENTER
-        imageAlpha = if (mChecked) 255 else 64
+        // Touch Effect
+        mAlphaAnimator = ObjectAnimator.ofInt(this, "imageAlpha", 0, 0).apply {
+            duration = 150
+        }
+        setOnTouchListener { v, ev ->
+            when (ev.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    if (v.isEnabled) {
+                        mPressed = true
+                        animateAlpha()
+                        return@setOnTouchListener true
+                    }
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    if (v.isEnabled) {
+                        mPressed = false
+                        animateAlpha()
+                    }
+                }
+                MotionEvent.ACTION_UP -> {
+                    if (v.isEnabled) {
+                        mPressed = false
+                        animateAlpha()
+                        v.performClick()
+                    }
+                }
+            }
+            false
+        }
+        imageAlpha = decideAlpha()
         setOnClickListener { _ -> toggle() }
     }
     override fun isChecked(): Boolean = mChecked
     override fun setChecked(checked: Boolean) {
         mChecked = checked
-        imageAlpha = if (mChecked) 255 else 64
+        animateAlpha()
+        onToggle?.invoke(this, isChecked)
     }
     override fun toggle() {
         isChecked = !isChecked
-        onToggle?.invoke(this, isChecked)
+    }
+    private fun animateAlpha() {
+        mAlphaAnimator.cancel()
+        mAlphaAnimator.setIntValues(imageAlpha, decideAlpha())
+        mAlphaAnimator.start()
+    }
+    private fun decideAlpha(): Int {
+        return if (mChecked) {
+            if (mPressed) 32 else 255
+        } else {
+            if (mPressed) 32 else 64
+        }
     }
 }
